e31be7a45ef2acfc7a925351a79a4ca057b15c67
张欢 <zhang.huan@shipout.cn>
2020-07-03 22:28:01 +0800

    build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+import './assets/style/common.less'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import ElementUI from 'element-ui'
+import axiosInit from './assets/js/axiosInit.js'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.use(ElementUI)
+Vue.axios = Vue.prototype.$axios = axiosInit
 
 Vue.config.productionTip = false
 
